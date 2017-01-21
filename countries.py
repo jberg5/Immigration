38cd9547b2ef12a4680857def0352558d3d98a5e
Jonathan Berg <Jonathan Berg>
2017-01-21 16:50:52 -0500

    Setup country and World classes

---
+++ countries.py
@@ -0,0 +1,24 @@
+import csv
+
+class Country:
+    """A country represented by a list of long/lat coordinates, along with a dictionary of immigration data."""
+    def __init__(self,name,coords,inflow):
+        self.name = name
+        self.coords = coords
+        self.data = {yr:ppl for yr,ppl in zip(list(range(1999,2015)),inflow)}
+    def change(self,year):
+        return (self.data[year-1] + self.data[year] + self.data[year+1])/3
+
+
+class World:
+    def __init__(self):
+        self.immdat = 'ImmigrationData.csv'
+        self.countries = []
+
+    def initialize():
+        with open(self.immdat) as fin:
+            csvobj = csv.reader(fin)
+            for row in csvobj:
+                name = row[0]
+                inflow = [int(x.replace(',','')) if x != 'X' and x != ' D ' and x != 'D' and x != '-' and x != ' - ' and x != '' else 0 for x in row[1:]]
+                self.countries.append(Country(name,None,inflow))
